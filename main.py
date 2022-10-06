2e070bd39bc9249829d99cb8af122f6291017af1
eVoinho <igormilczarski@gmail.com>
2022-10-06 12:07:31 +0200

    Looks like work for 3 is done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import json
+import csv
 from wolf import Wolf
 from sheep import Sheep
 
@@ -15,15 +16,25 @@ def create_sheep_flock(size, pos_limit):
 def create_dict_to_json_file(sheep_list, wolf, round_number):
     sheep_pos_list = []
     for sheep in sheep_list:
-        sheep_pos_list.append([sheep.get_x(), sheep.get_y()])
+        if sheep.get_x() is not None:
+            sheep_pos_list.append([sheep.get_x(), sheep.get_y()])
+        else:
+            sheep_pos_list.append(None)
     return {"round_no": round_number, "wolf_pos": [wolf.get_x(), wolf.get_y()], "sheep_pos": sheep_pos_list}
 
 
+def count_alive_sheep(sheep_list):
+    num_of_alive = 0
+    for sheep in sheep_list:
+        if sheep.get_x() is not None:
+            num_of_alive += 1
+    return num_of_alive
 
 
-## TODO: Zapytać kiedy ma być zapisywana informacja do jsona i kiedy mają być wświtalanae napisy (po ruchu czy przed ruchem)
+# TODO: czy dane o pozycji mają być
 def run_simulation(sheep_move, wolf_move, max_round, sheep_list, wolf):
     list_of_dicts = []
+    list_for_alive_csv = []
     for i in range(max_round):
         count = 0
         for sheep in sheep_list:
@@ -31,15 +42,24 @@ def run_simulation(sheep_move, wolf_move, max_round, sheep_list, wolf):
                 sheep.move(sheep_move)
                 count += 1
         if count == 0:
+            print(f"Number of round: {i}")
+            print(f"Current wolf position is: {round(wolf.get_x(), 3)} {round(wolf.get_y(), 3)}")
+            print(f"There is {count} sheep left")
+            list_of_dicts.append(create_dict_to_json_file(sheep_list, wolf, i))
             print("<<<===>>> Wolf has eaten all sheep <<<===>>>")
             break
-        print(f"Current wolf position is: {round(wolf.get_x(), 3)} {round(wolf.get_y(), 3)}")
-        print(f"Number of round: {i}")
-        print(f"Before wolf's move, there was {count} sheep")
-        list_of_dicts.append(create_dict_to_json_file(sheep_list, wolf, i))
         wolf.try_to_eat_sheep(sheep_list, wolf_move)
-    with open("pos.json", "w") as log:
-        json.dump(list_of_dicts, log, indent=4)
+        print(f"Number of round: {i}")
+        print(f"Current wolf position is: {round(wolf.get_x(), 3)} {round(wolf.get_y(), 3)}")
+        print(f"There is {count} sheep left")
+        list_of_dicts.append(create_dict_to_json_file(sheep_list, wolf, i))
+        list_for_alive_csv.append([i, count_alive_sheep(sheep_list)])
+    with open("pos.json", "w") as f:
+        json.dump(list_of_dicts, f, indent=4)
+    with open("alive.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        # writer.writerow(["round_number", "sheep_alive"])
+        writer.writerows(list_for_alive_csv)
 
 
 if __name__ == "__main__":
@@ -54,6 +74,3 @@ if __name__ == "__main__":
     wolf = Wolf(0, 0)
 
     run_simulation(sheep_move_dist, wolf_move_dist, max_rounds, sheep, wolf)
-
-
-
